1844eb8a36b1e28e39b16bdaab825539d99f4d37
Vitali Rapalis <rapalvi1@P7540-012WX.ag.isb.de>
2022-07-24 11:48:57 +0200

    set up server oauth2

--- libs/server-starter/src/main/java/com/vrapalis/www/libs/oauth2/server/core/config/OAuth2ServerCoreAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.vrapalis.www.libs.oauth2.server.core.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.vrapalis.www.libs.oauth2.server.core.domain")
-public class OAuth2ServerCoreAutoConfiguration {
-}
